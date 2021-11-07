a535dc897da5ddd8773e02872b2e4bbf13555157
backmeupplz <backmeupplz@gmail.com>
2021-11-07 15:44:23 -0800

    try to work around flood wait

--- src/helpers/checker.ts
+++ src/helpers/checker.ts
@@ -4,7 +4,7 @@ import delay from '@/helpers/delay'
 
 const initialCheckTimeout = 5
 const checkingInterval = 10 * 60 // once every 10 minutes
-const checkStep = 30
+const checkStep = 10
 
 export default function startChecking() {
   setTimeout(() => void check(), initialCheckTimeout * 1000)
@@ -24,7 +24,7 @@ async function check() {
     while (bots.length) {
       const botsToCheck = bots.splice(0, checkStep)
       await Promise.all(botsToCheck.map((bot) => checkBotAndDoSendout(bot)))
-      await delay(1)
+      await delay(2)
     }
     console.log('Finished checking bots')
   } catch (error) {
